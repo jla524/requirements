4a0d09befc265fbdc5c78e8175fd2ef4b31f59c8
Jacky Lee <jla524@sfu.ca>
2023-01-13 08:57:31 -0800

    Remove equal sign from pyproject's version number

--- src/convert.py
+++ src/convert.py
@@ -12,10 +12,11 @@ class RequirementsConverter:
     """
     @description: a class containing scripts for conversion
     """
+
     def __init__(self, project_dir: str, with_version: bool = True) -> None:
         self.__project_dir = Path(project_dir).resolve(strict=True)
-        self.__source = self.__project_dir / 'pyproject.toml'
-        self.__target = self.__project_dir / 'requirements.txt'
+        self.__source = self.__project_dir / "pyproject.toml"
+        self.__target = self.__project_dir / "requirements.txt"
 
         self.__with_version = with_version
         self.__dependencies = self.__load_dependencies()
@@ -26,8 +27,8 @@ class RequirementsConverter:
         @description: get a list of dependencies from pyproject.toml
         """
         content = toml.load(self.__source)
-        packages = content['tool']['poetry']['dependencies']
-        packages.pop('python', None)
+        packages = content["tool"]["poetry"]["dependencies"]
+        packages.pop("python", None)
         return packages
 
     def __make_requirements(self) -> str:
@@ -35,11 +36,13 @@ class RequirementsConverter:
         @description: convert the list of dependencies to requirements format
         """
         if self.__with_version:
-            items = [f'{package}=={version.strip("^")}'
-                     for package, version in self.__dependencies.items()]
+            items = [
+                f'{package}=={version.strip("^=")}'
+                for package, version in self.__dependencies.items()
+            ]
         else:
             items = list(self.__dependencies.keys())
-        return '\n'.join(items)
+        return "\n".join(items)
 
     def get_dependencies(self) -> dict[str, str]:
         """
@@ -57,14 +60,14 @@ class RequirementsConverter:
         """
         @description: write requirements to requirements.txt
         """
-        with open(self.__target, 'w', encoding='UTF-8') as file:
+        with open(self.__target, "w", encoding="UTF-8") as file:
             file.write(self.__requirements)
         print(f"requirements.txt has been stored in {self.__project_dir}")
 
 
 @click.command()
-@click.argument('project_dir', default='.')
-@click.option('--version/--noversion', default=True)
+@click.argument("project_dir", default=".")
+@click.option("--version/--noversion", default=True)
 def main(project_dir, version):
     """
     @description: main function to perform the conversion
@@ -73,5 +76,5 @@ def main(project_dir, version):
     converter.write_requirements()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
