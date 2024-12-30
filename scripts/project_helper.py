189a892f98d4e3347d44abae916d96b4e3e244c4
Jacky Lee <jla524@sfu.ca>
2024-12-29 19:14:04 -0800

    update: use double quotes

--- scripts/project_helper.py
+++ scripts/project_helper.py
@@ -4,7 +4,7 @@ import subprocess as sp
 
 def run_tests():
     try:
-        sp.run('pytest --capture=tee-sys', check=True, shell=True)
+        sp.run("pytest --capture=tee-sys", check=True, shell=True)
     except sp.CalledProcessError as error:
         print(str(error))
         sys.exit(1)
